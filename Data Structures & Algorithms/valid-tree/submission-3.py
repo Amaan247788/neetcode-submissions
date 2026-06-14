8a4c916e0d5f9549f70f9083334b15febd651afc
Amaan247788 <145923599+Amaan247788@users.noreply.github.com>
2026-06-14 22:53:26 +0300

    Add: valid-tree - submission-3

---
+++ Data Structures & Algorithms/valid-tree/submission-3.py
@@ -0,0 +1,33 @@
+from collections import defaultdict
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        # so no child can have two parents
+        if not n:
+            return True
+        
+        visited = set()
+        neighbors = defaultdict(list[int])
+
+        for par,child in edges:
+            neighbors[par].append(child)
+            neighbors[child].append(par)
+
+        def dfs(i: int, prev: int) -> bool:
+            # if node has no neighbors
+            if i in visited:
+                return False
+            # if all its neighbors visited or prev
+            visited.add(i)
+            if i in neighbors:
+                for neighbor in neighbors[i]:
+                    if neighbor != prev:
+                        if not dfs(neighbor, i):
+                            return False
+            return True
+        
+        if not dfs(0, -1):
+            return False
+
+        return len(visited) == n
+            
+
